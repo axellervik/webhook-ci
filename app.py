73e573677ce4f425353a4cf3b4ab0a96ae2a5e70
axellervik <axel.lervikgmail.com>
2023-02-05 09:31:46 +0100

    Init app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+usersList = ['Aaron', 'Bianca', 'Cat', 'Danny', 'Elena']
+
+@app.route('/users', methods=['GET'])
+def users():
+    return jsonify({ 'users': [user for user in usersList] })
+
+@app.route('/user/<int:id>', methods=['GET'])
+def userById(id):
+    return jsonify({ 'username': usersList[id]  })
+
+@app.route('/user/<string:name>', methods=['GET'])
+def getUserByName(name):
+    # Show some user information
+    return "Some info"
+
+@app.route('/user/<string:name>', methods=['POST'])
+def addUserByName(name):
+    usersList.append(name)
+    return jsonify({ 'message': 'POST'})
+
+# main driver function
+if __name__ == '__main__':
+    app.run()
